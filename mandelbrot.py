e1ff55f9092302b2cde87aaec7249c2fa4223f21
Rhys Tyne <tynerhys@gmail.com>
2024-12-01 16:51:18 +1000

    added comment

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,3 +1,5 @@
+# ngl this code is based off of chatgpt query with some small changes to improve speed
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,14 +14,14 @@ def fractal_set(xmin, xmax, ymin, ymax, width, height, max_iter):
 
     for i in range(max_iter):
         mask = np.abs(Z) <= 2
-        Z[mask] = Z[mask] ** 3 - (abs(Z[mask].real) - 1j*abs(Z[mask].imag)) ** 2 + C[mask]
+        Z[mask] = Z[mask] ** 2 - np.sin(Z[mask]) ** (abs(Z[mask].real) - 1j*abs(Z[mask].imag)) ** 2 + C[mask]
         #Z[mask] = Z[mask] ** 6 - Z[mask] ** 2 + C[mask]
         fractal[mask] += 1
 
     return fractal
 
 # Parameters
-xmin, xmax, ymin, ymax = -1.5, 1.5, -1.5, 1.5
+xmin, xmax, ymin, ymax = -1, 3, -2, 2
 width, height = 800, 800
 max_iter = 100
 
@@ -30,8 +32,8 @@ fractal = fractal_set(xmin, xmax, ymin, ymax, width, height, max_iter)
 plt.figure(figsize=(10, 10))
 plt.imshow(fractal, extent=(xmin, xmax, ymin, ymax), cmap="hot", origin="lower")
 
-plt.colorbar(label="Iteration count")
-name = "z = $z^{6}$ - $z^{2}$ + c"
+#name = "z = $z^{6}$ - $z^{2}$ + c"
+name = "z = $z^{2}$ - sin(z)$^{(|Re(z)| - i|Im(z)|)^{2}}$ + c"
 plt.title(name)
 plt.xlabel("Re")
 plt.ylabel("Im")
